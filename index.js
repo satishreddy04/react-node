1172913add9743a93955088c80edd71adabc8f56
Kumar Reddy Burugu <satish.kumarreddyburugu@grainger.com>
2020-05-06 16:57:58 -0500

    commented all codes to test the deployment in heroku

--- index.js
+++ index.js
@@ -1,57 +1,59 @@
 // express frame work is the popular frame work for Node js application and its widely used
 const express = require('express');
 // mongoose  is for using Mongo DB to store the data
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 //body parser is used to parse the incoming request bodies before any request made
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 // dotenv is used for getting the env files from local 
-const dotenv = require("dotenv");
+//const dotenv = require("dotenv");
 
-const passport = require("passport");
+//const passport = require("passport");
 
-const session = require("express-session");
+//const session = require("express-session");
 
 const app = express();
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
-app.use(passport.initialize());
+//app.use(passport.initialize());
 
-app.use(passport.session());
+//app.use(passport.session());
 
-app.use(session({
-  secret: 'process.env.SESSION_SECRET',
-  resave: true,
-  saveUninitialized: true,
-}));
+// app.use(session({
+//   secret: 'process.env.SESSION_SECRET',
+//   resave: true,
+//   saveUninitialized: true,
+// }));
 
-dotenv.config()
+//dotenv.config()
 
 // IMPORT MODELS
-require('./models/Products');
+// require('./models/Products');
 
 //IMPORT ROUTES
-require('./routes/productRoutes')(app);
+// require('./routes/productRoutes')(app);
 
 // this code does that it will redirect all request to front end app unless if we specify any routes
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  const path = require('path');
-  app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+//   const path = require('path');
+//   app.get('*', (req,res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   })
 
-}
-
-
-
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
-
-mongoose.Promise = global.Promise;
+// }
 
 
+
+// mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongoose.Promise = global.Promise;
+
+app.get('/',(req,res)=>{
+  res.send({"Welcome":"to Node and React Project"})
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
